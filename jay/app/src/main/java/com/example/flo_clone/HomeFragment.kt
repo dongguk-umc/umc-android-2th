97d006e57d20d3a11e003e3edc397f7290f0ecd3
102sae <95170874+102sae@users.noreply.github.com>
2022-04-01 23:14:11 +0900

    2nd week

---
+++ jay/app/src/main/java/com/example/flo_clone/HomeFragment.kt
@@ -0,0 +1,31 @@
+package com.example.flo_clone
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.flo_clone.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.homePannelTodaymusicAlbumcover1Iv.setOnClickListener {
+            (context as MainActivity).supportFragmentManager.beginTransaction()
+                .replace(R.id.main_frm , AlbumFragment())
+                .commitAllowingStateLoss()
+        }
+
+        return binding.root
+    }
+
+
+}
